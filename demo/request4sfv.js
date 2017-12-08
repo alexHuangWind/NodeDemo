c6501129f662c5537091e53d88448cb50dec3880
changyu <alexchyandrioid@gmail.com>
2017-12-09 08:14:09 +1300

    add log

--- demo/request4sfv.js
+++ demo/request4sfv.js
@@ -3,8 +3,8 @@ request = require('request');
 nodemailer = require('nodemailer');
 count = 0;
 doReport();
-
-function doEmail(msg) {
+function doEmail(to,title,body) {
+     console.log('begin doEmail  ' + 'to = '+to + 'title = '+title+ 'body = '+body);
     //2创建一个传输对象
     let transporter = nodemailer.createTransport({
         host: 'smtp.sina.com',
@@ -17,12 +17,12 @@ function doEmail(msg) {
     });
     //3、用unicode字符设置电子邮件数据
     let mailOptions = {
-        from: '"alex" <alexusedforsfv@sina.com>', //邮件来源
-        to: 'nzleo@foxmail.com', //邮件发送到哪里，多个邮箱使用逗号隔开
-        //to: 'alexchyandroid@gmail.com',
-        subject: msg, // 邮件主题
-        text: 'Hello world ?', // 存文本类型的邮件正文
-        html: '<b>Hello world ?</b>' // html类型的邮件正文
+        from: '"alex" <alexusedforsfv@sina.com>',, //邮件来源
+        //to: 'nzleo@foxmail.com', //邮件发送到哪里，多个邮箱使用逗号隔开
+        to: to,
+        subject: title, // 邮件主题
+        text: body, // 存文本类型的邮件正文
+        html: body // html类型的邮件正文
     };
     //4、使用第二步定义的传输对象发送邮件
     transporter.sendMail(mailOptions, (error, info) => {
@@ -33,17 +33,17 @@ function doEmail(msg) {
     });
 }
 
-
 function doRequest() {
     request('https://www.immigration.govt.nz/new-zealand-visas/apply-for-a-visa/about-visa/silver-fern-job-search-work-visa', function(error, response, body) {
+        console.log('error = ' + error +'response = ' + response +'body = ' + body );
         if (!error && response.statusCode == 200) {
             if (body.indexOf("This visa is closed and will open on") != -1) {
                 console.log('closed');
                 count++;
-                setTimeout(doRequest, 3000);
+                setTimeout(doRequest, 10*1000);
             } else {
                 console.log('包含 opened');
-                doEmail('success');
+                doEmail('alexchyandroid@gmail.com',response.body,'success');
                 count = 0;
             }
         }
@@ -51,10 +51,8 @@ function doRequest() {
 }
 
 function doReport() {
-    doEmail('已发送信息数：' + count);
-    setTimeout(doReport, 60 * 60 * 1000);
+    doEmail('alexchyandroid@gmail.com','已发送信息数：'+count);
+    setTimeout(doReport, 30*60*1000);
 }
-
-
 
 doRequest();
